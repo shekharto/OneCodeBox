7ec6f89894ae80a065a9e74487f622b39ac8acb5
shekharto <shekhar.parate@yahoo.com>
2021-05-30 17:25:47 +0530

    Update AppSettings.cs

--- CrudApi_Microservices/CRUDApi.Core/Common/AppSettings.cs
+++ CrudApi_Microservices/CRUDApi.Core/Common/AppSettings.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
